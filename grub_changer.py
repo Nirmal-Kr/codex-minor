93edd7ab9d9e8f7a248052124a02324e37565915
AshisPadhi <ashispadhi08@gmail.com>
2018-01-12 22:37:01 +0530

    Update grub_changer.py

--- grub_changer.py
+++ grub_changer.py
@@ -3,7 +3,7 @@ import random
 
 print('Beginning file download with requests')
 fst='https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-'
-randi=random.randint(7000,8000)
+randi=random.randint(1,610000)
 url = fst+str(randi)+'.jpg'  
 
 r = requests.get(url)
