72121fdac6f48781fc49a8eb6ceb80685bc506aa
Nirmal Kumar Bhakat <nbhakat2404@gmail.com>
2018-01-14 10:49:38 +0530

    Update grub_changer.py

--- grub_changer.py
+++ grub_changer.py
@@ -7,8 +7,12 @@ randi=random.randint(1,610000)
 url = fst+str(randi)+'.jpg'  
 
 r = requests.get(url)
-
-with open('/home/ashispadhi/Downloads/cat.jpg', 'wb') as f:  
+while(r.headers['content-type']!='image/jpeg'):
+	fst='https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-'
+	randi=random.randint(56000,610000)
+	url = fst+str(randi)+'.jpg'  
+	r = requests.get(url)
+with open('/home/nirmal/Downloads/cat.jpg', 'wb') as f:  
 	f.write(r.content)
 
  #Retrieve HTTP meta-data
